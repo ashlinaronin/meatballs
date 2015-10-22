89b8f3f59c467b35e3e17d12cb3aba3bfad44b6e
Ashlin Aronin & Deron Johnson <student@epicodus.com>
2015-10-21 17:02:26 -0700

    Get lighting functioning

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Ashlin and Deron are playing with three.js.
+
+It is very slow. Also Mitch thanks for modeling the flesh cube.
--- js/basic.js
+++ js/basic.js
@@ -1,39 +1,64 @@
+// if (!Detector.webgl) Detector.addGetWebGLMessage();
+
+var viewport, stats;
+
 var scene, camera, renderer, loader, mesh, material, cube;
 var group;
 
 init();
 render();
 
+
 function init() {
-  scene = new THREE.Scene();
 
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+  // camera.aspect = window.innerWidth / window.innerHeight;
 
   // Move back a little bit so the cube isn't in our face!!
-  camera.position.z = 5;
+  camera.position.z = 25;
 
-  // Set background to have transparency- alpha: true
-  renderer = new THREE.WebGLRenderer({alpha: true});
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  controls = new THREE.OrbitControls(camera);
+  controls.addEventListener('change', render);
 
-  document.getElementById("viewport").appendChild(renderer.domElement);
+  scene = new THREE.Scene();
 
   //directional lighting
-  var directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(1, 1, 1).normalize();
+  var directionalLight = new THREE.DirectionalLight(0xffffff, .5);
+  directionalLight.position.set(1, 1, 1);
+  directionalLight.castShadow = true;
+  directionalLight.shadowDarkness = 0.5;
   scene.add(directionalLight);
 
-  //ambient lighting
-  var ambientLight = new THREE.AmbientLight(0xbbbbbb);
-  scene.add(ambientLight);
+  var spotLigh = new THREE.SpotLight(0xffffff, 1, 200, 20, 10);
+  spotLigh.position.set(0,100,0);
+  scene.add(spotLigh);
+  scene.add(new THREE.PointLightHelper(spotLigh, 1));
 
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
+
+  // Set background to have transparency- alpha: true
+  renderer = new THREE.WebGLRenderer({ antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // renderer.shadowMapEnabled = true;
+  // renderer.shadowMapType = THREE.PCFSoftShadowMap;
+
+  viewport = document.getElementById("viewport").appendChild(renderer.domElement);
+
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.top = '0px';
+  stats.domElement.style.zIndex = 100;
+  document.getElementById("container").appendChild(stats.domElement);
+
+  window.addEventListener('resize', onWindowResize, false);
+
+
+  var geometry = new THREE.BoxGeometry(3, 3, 3);
   var loader = new THREE.TextureLoader();
 
   loader.load(
     'img/tex.jpg',
     function(texture) {
-      material = new THREE.MeshBasicMaterial({ map: texture });
+      material = new THREE.MeshPhongMaterial({ map: texture });
       cube = new THREE.Mesh(geometry, material);
       scene.add(cube);
     },
@@ -46,9 +71,18 @@ function init() {
   );
 }
 
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+}
 
 function render() {
   requestAnimationFrame(render);
-  cube.rotation.y += 0.05;
+  // cube.rotation.y += 0.05;
+  // cube.rotation.x += 0.01;
   renderer.render(scene, camera);
+  stats.update();
 }
